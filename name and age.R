d00c8e98521444052f47cbcb91b112904b563d21
sudharsa07 <sudharsons1077.sse@saveetha.com>
2025-02-08 11:25:36 +0530

    Delete name and age.R

--- name and age.R
+++
@@ -1,5 +0,0 @@
-name <- readline(prompt = "Enter your name: ")
-age <- as.integer(readline(prompt = "Enter your age: "))
-cat("Name:", name, "\n")
-cat("Age:", age, "\n")
-cat("R version:", R.version$version.string, "\n")
