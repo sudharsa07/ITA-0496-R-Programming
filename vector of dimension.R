d0d90897203d8b5e3d2eaceeac9443ade4cda903
sudharsa07 <sudharsons1077.sse@saveetha.com>
2025-02-13 09:07:16 +0530

    UNIT 02

---
+++ vector of dimension.R
@@ -0,0 +1,6 @@
+my_array <- array(1:24, dim = c(2, 3, 4), 
+dimnames = list(Row = c("Row1", "Row2"), 
+ Column = c("Col1", "Col2", 
+"Col3"), Matrix = c("M1", "M2", 
+"M3", "M4"))) 
+print(my_array) 
