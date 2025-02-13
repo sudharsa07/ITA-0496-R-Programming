2ced3e0f334da41205fc9178589c242b8b666d37
sudharsa07 <sudharsons1077.sse@saveetha.com>
2025-02-13 09:27:47 +0530

    UNIT 02

---
+++ print array.R
@@ -0,0 +1,6 @@
+vector1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9) 
+vector2 <- c(10, 11, 
+12, 13, 14, 15, 16, 17, 18) 
+array_data <- array(c(vector1, vector2), dim = c(3, 3, 2)) 
+cat("The Array is: \n") 
+print(array_data) 
