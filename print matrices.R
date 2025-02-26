6281bbd36bb30aebfad0093173acf522ac919699
sudharsa07 <sudharsons1077.sse@saveetha.com>
2025-02-26 20:21:24 +0530

    UNIT 03

---
+++ print matrices.R
@@ -0,0 +1,7 @@
+vector1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+vector2 <- c(10, 11, 12, 13, 14, 15, 16, 17, 18)
+matrix1 <- matrix(vector1, 3, 3)
+matrix2 <- matrix(vector2, 3, 3)
+array_matrices <- array(c(matrix1, matrix2), dim = c(3, 3, 2))
+print(array_matrices[2, , 2])
+print(array_matrices[3, 3, 1])
