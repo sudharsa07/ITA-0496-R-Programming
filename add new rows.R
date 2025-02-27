91c6ec24f83640931ece05fac93ab5cc7ec92995
sudharsa07 <sudharsons1077.sse@saveetha.com>
2025-02-27 17:18:00 +0530

    UNIT 03

---
+++ add new rows.R
@@ -0,0 +1,12 @@
+exam_data <- data.frame( 
+name = c('Anastasia', 'Dima'), 
+score = c(12.5, 9), 
+attempts = c(1, 3), 
+qualify = c('yes', 'no') 
+) 
+new_exam_data <- data.frame( 
+name = c('Robert', 'Sophia'), 
+score = c(10.5, 9), 
+attempts = c(1, 3), 
+qualify = c('yes', 'no') 
+) 
