fbf713ad55589d0bff9b077dbe846a656c276a26
sudharsa07 <sudharsons1077.sse@saveetha.com>
2025-03-20 19:07:31 +0530

    UNIT 04

---
+++ statistics on test scores.R
@@ -0,0 +1,3 @@
+scores <- c(85, 90, 78, 92, 88, 76, 95, 89, 84, 91, 87, 82, 90, 93, 88, 85, 77, 94, 80, 79)
+summary(scores)
+cat("Range:", range(scores), "Variance:", var(scores), "SD:", sd(scores))
