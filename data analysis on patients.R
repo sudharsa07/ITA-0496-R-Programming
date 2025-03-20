36dfdaab1dc0b13fe2d7d816c7cfd377d22cbd3c
sudharsa07 <sudharsons1077.sse@saveetha.com>
2025-03-20 19:25:12 +0530

    UNIT 04

---
+++ data analysis on patients.R
@@ -0,0 +1,4 @@
+patients <- data.frame(Age = c(25,30,35,40,45,50,55,60,65,70), BP = c(120,125,130,128,135,140,138,145,150,155))
+
+cat("Age - Mean:", mean(patients$Age), "SD:", sd(patients$Age), "\n")
+cat("BP - Mean:", mean(patients$BP), "SD:", sd(patients$BP), "\n")
